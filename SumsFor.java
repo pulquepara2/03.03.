63492e54d336649f8fc22c29bc0101054c5a0e39
pulquepara2 <123812696+pulquepara2@users.noreply.github.com>
2023-03-03 12:11:21 +0100

    Add files via upload

---
+++ SumsFor.java
@@ -0,0 +1,16 @@
+public class SumsFor {
+    public static void main(String[] args) {
+
+        int sum = 0;
+        for (int i = 50; i <= 100; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+        for (int i = 0; 1 < 5; i++) {
+
+            System.out.println("Yay for loops are cool");
+        }
+    }
+}
+
